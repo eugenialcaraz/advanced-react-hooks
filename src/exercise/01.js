19fb59059ceb5444e6ce8a2b48ef902073d93703
Eugenia Alcaraz <eugenia.alcaraz@rootstrap.com>
2023-04-27 10:30:46 -0500

    Excercises of advanced hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,18 +1,113 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
+// import * as React from 'react'
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, setCount] = React.useReducer(countReducer, initialCount)
+
+//   function countReducer(state, newState) {
+//     return newState
+//   }
+
+//   const increment = () => setCount(count + step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// function App() {
+//   return <Counter />
+// }
+
+// export default App
+
+//Extra credit 1
+// import * as React from 'react'
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, setCount] = React.useReducer(countReducer, initialCount)
+
+//   function countReducer(state, step) {
+//     return state + step
+//   }
+
+//   const increment = () => setCount(step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// function App() {
+//   return <Counter />
+// }
+
+// export default App
+
+//Extra credit 2
+// import * as React from 'react'
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+
+//   function countReducer(state, newState) {
+//     return {...state, ...newState}
+//   }
+
+//   const {count} = state
+//   const increment = () => setState({count: count + step})
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// function App() {
+//   return <Counter />
+// }
+
+// export default App
+
+//Extra credit 3
+// import * as React from 'react'
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+
+//   function countReducer(state, newState) {
+//     return {
+//       ...state,
+//       ...(typeof newState === 'function' ? newState(state) : newState),
+//     }
+//   }
+
+//   const {count} = state
+//   const increment = () =>
+//     setState(currentState => ({count: currentState.count + step}))
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// function App() {
+//   return <Counter />
+// }
+
+// export default App
+
+//Extra credit 4
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  function countReducer(state, newState) {
+    return (
+      newState.type === 'INCREMENT' && {
+        ...state,
+        count: state.count + newState.step,
+      }
+    )
+  }
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
